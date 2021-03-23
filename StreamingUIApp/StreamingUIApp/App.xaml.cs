ffe724fcee36279883d3ef546ed2eaad532654b7
Caleb Okogbe <okogbecaleb@gmail.com>
2021-03-23 12:24:04 +0100

    App center distribute

--- StreamingUIApp/StreamingUIApp/App.xaml.cs
+++ StreamingUIApp/StreamingUIApp/App.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Distribute;
 
 namespace StreamingUIApp
 {
@@ -15,6 +17,7 @@ namespace StreamingUIApp
 
         protected override void OnStart()
         {
+            AppCenter.Start("android=1f4e26be-66ef-49a5-b6ad-82baa511d8dd", typeof(Distribute));
         }
 
         protected override void OnSleep()
